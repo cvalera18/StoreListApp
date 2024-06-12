4d4ced865f013f9cc2763d094a51bc3074affc3b
cvalera18 <ing.cvalera@gmail.com>
2024-06-12 11:46:06 -0400

    First Commit Add Dependencies

---
+++ app/src/main/java/com/example/storeslist/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.storeslist
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.commit
+import com.example.storeslist.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        // Inflar el layout utilizando ViewBinding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+//                replace(binding.containerView.id, ListFragment())
+            }
+        }
+    }
+}
