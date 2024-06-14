737bfb1e7576a8b1d1cea870ba8a56ec9b5d9b48
cvalera18 <ing.cvalera@gmail.com>
2024-06-14 15:02:31 -0400

    Repository's test

--- app/src/test/java/com/example/storeslist/data/repository/StoreRepositoryImplTest.kt
+++ app/src/test/java/com/example/storeslist/data/repository/StoreRepositoryImplTest.kt
@@ -140,4 +140,172 @@ class StoreRepositoryImplTest {
             // then
             coVerify { localDataSource.saveStores(storeList) }
         }
+
+    @Test
+    fun `getStores should call getNextStorePage from remoteDataSource`() = runTest {
+        // given
+        every { networkUtils.isInternetAvailable() } returns true
+
+        val storeDataList = listOf(
+            StoreData(
+                id = "1",
+                type = "store",
+                attributes = StoreAttributes(
+                    name = "Store 1",
+                    code = "1",
+                    full_address = "Address 1"
+                )
+            ),
+            StoreData(
+                id = "2",
+                type = "store",
+                attributes = StoreAttributes(
+                    name = "Store 2",
+                    code = "2",
+                    full_address = "Address 2"
+                )
+            )
+        )
+        val nextPage = "NextPageUrl"
+        val response = StoreResponse(
+            data = storeDataList,
+            meta = Meta(Pagination(current_page = 1, total = 121, per_page = 10)),
+            links = Links(
+                prev = null,
+                next = nextPage,
+                first = "first",
+                last = "last",
+                self = "self"
+            )
+        )
+        coEvery { remoteDataSource.getInitialStores() } returns response
+        coEvery { remoteDataSource.getNextStorePage(nextPage) } returns response
+
+        // when
+        storeRepository.getStores()
+        storeRepository.getStores()
+
+        // then
+        coVerify { remoteDataSource.getNextStorePage(nextPage) }
+    }
+
+    @Test
+    fun `When getNextStorePage is successful, saveStores in localDataSource should be called`() =
+        runTest {
+            // given
+            every { networkUtils.isInternetAvailable() } returns true
+
+            val storeDataList = listOf(
+                StoreData(
+                    id = "1",
+                    type = "store",
+                    attributes = StoreAttributes(
+                        name = "Store 1",
+                        code = "1",
+                        full_address = "Address 1"
+                    )
+                ),
+                StoreData(
+                    id = "2",
+                    type = "store",
+                    attributes = StoreAttributes(
+                        name = "Store 2",
+                        code = "2",
+                        full_address = "Address 2"
+                    )
+                )
+            )
+            val nextPage = "NextPageUrl"
+            val response = StoreResponse(
+                data = storeDataList,
+                meta = Meta(Pagination(current_page = 1, total = 121, per_page = 10)),
+                links = Links(
+                    prev = null,
+                    next = nextPage,
+                    first = "first",
+                    last = "last",
+                    self = "self"
+                )
+            )
+            val storeList = response.data.map { it.toStore() }
+            coEvery { remoteDataSource.getInitialStores() } returns response
+            coEvery { remoteDataSource.getNextStorePage(nextPage) } returns response
+
+            // when
+            storeRepository.getStores()
+            storeRepository.getStores()
+
+            // then
+            coVerify { localDataSource.saveStores(storeList) }
+        }
+
+    @Test
+    fun `when nextPage is null, then don't call remoteDataSource`() =
+        runTest {
+            // given
+            every { networkUtils.isInternetAvailable() } returns true
+            val storeDataList = listOf(
+                StoreData(
+                    id = "1",
+                    type = "store",
+                    attributes = StoreAttributes(
+                        name = "Store 1",
+                        code = "1",
+                        full_address = "Address 1"
+                    )
+                ),
+                StoreData(
+                    id = "2",
+                    type = "store",
+                    attributes = StoreAttributes(
+                        name = "Store 2",
+                        code = "2",
+                        full_address = "Address 2"
+                    )
+                )
+            )
+
+            val response = StoreResponse(
+                data = storeDataList,
+                meta = Meta(Pagination(current_page = 1, total = 121, per_page = 10)),
+                links = Links(
+                    prev = null,
+                    next = null,
+                    first = "first",
+                    last = "last",
+                    self = "self"
+                )
+            )
+            val storeList = response.data.map { it.toStore() }
+            coEvery { remoteDataSource.getInitialStores() } returns response
+
+            // when
+            storeRepository.getStores()
+            storeRepository.getStores()
+
+            // then
+            coVerify(exactly = 1) { remoteDataSource.getInitialStores() }
+            coVerify(exactly = 0) { remoteDataSource.getNextStorePage(any()) }
+            coVerify(exactly = 1) { localDataSource.saveStores(storeList) }
+        }
+
+    @Test(expected = Exception::class)
+    fun `when remote data source throw an error, then propagate the error`() =
+        runTest {
+            // given
+            every { networkUtils.isInternetAvailable() } returns true
+            val exception = Exception("Error")
+            coEvery { remoteDataSource.getInitialStores() } throws exception
+            // then
+            storeRepository.getStores()
+        }
+
+    @Test(expected = Exception::class)
+    fun `when Internet is not available, then propagate the error`() =
+        runTest {
+            // given
+            every { networkUtils.isInternetAvailable() } returns false
+            // then
+            storeRepository.getStores()
+        }
 }
