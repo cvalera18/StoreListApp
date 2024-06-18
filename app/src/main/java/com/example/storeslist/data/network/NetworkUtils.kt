3796609eb6f157fb97efb77d1c474bd45bd458cc
cvalera18 <ing.cvalera@gmail.com>
2024-06-18 10:20:28 -0400

    Fix details

--- app/src/main/java/com/example/storeslist/data/network/NetworkUtils.kt
+++ app/src/main/java/com/example/storeslist/data/network/NetworkUtils.kt
@@ -6,7 +6,7 @@ import android.net.NetworkCapabilities
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
-class NetworkUtils @Inject constructor(@ApplicationContext private val context: Context) {
+class NetworkUtils (@ApplicationContext private val context: Context) {
     fun isInternetAvailable(): Boolean {
         val connectivityManager =
             context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
